f8ad14a69c4e7761929431a0ad2b8f64f95306a9
Francisco Rocha <fa676533@gmail.com>
2025-10-21 22:46:57 -0600

    update App.jsx and create file the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
+import Header from "./components/Header"
+import Guitarra from "./components/Guitarra"
+
 
 function App() {
   return (
     <>
-      <h1>Guitar LA</h1>
+        <Header />
+        <main className="container-xl mt-5">
+            <h2 className="text-center">Nuestra Colección</h2>
+
+            <div className="row mt-5">
+                <Guitarra />
+            </div>
+        </main>
+
+
+        <footer className="bg-dark mt-5 py-5">
+            <div className="container-xl">
+                <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+            </div>
+        </footer>
     </>
   )
 }
