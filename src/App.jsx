fc64350f58039ca16131bfc8493ec71ff85c229a
Francisco Rocha <fa676533@gmail.com>
2025-10-26 23:35:32 -0600

    update counter cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
+import { useState } from "react"
 import Guitarra from "./components/guitarra"
 import Header from "./components/Header"
-
+import { db } from "./data/db"
 
 function App() {
+
+    //UseSatate
+    const [data, setData] = useState(db);
+    const [cart, setCart] = useState([]);
+
+    //Agregar un item al carrito
+    function addToCart(item){
+
+        const itemExist = cart.findIndex((guitar) => guitar.id === item.id)
+
+        if(itemExist){
+            const updatedCart = [...cart];
+            updatedCart[itemExist].quantity++;
+            setCart(updatedCart);
+        }else{
+            // Agrega al carrito
+            item.quantity = 1;
+            setCart([...cart, item])
+        }
+
+    }
+
   return (
     <>
+        //Esto es un Componente
         <Header />
         <main className="container-xl mt-5">
             <h2 className="text-center">Nuestra Colección</h2>
 
             <div className="row mt-5">
-                <Guitarra />
+                  { data.map((guitar) =>(
+                    <Guitarra
+                    //Props de guitarra
+                      key={guitar.id}
+                      guitar={guitar}
+                      setCart={setCart}
+                      addToCart={addToCart}
+                    />
+                  ))}
             </div>
         </main>
 
